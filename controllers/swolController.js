4c327e4967fce3bee905f1ca140de1097d750401
Kim Gorcyca <kgorcyca@gmail.com>
2019-06-15 13:27:47 -0700

    updated naming, server.js, config, conrollers, and models framework

---
+++ controllers/swolController.js
@@ -0,0 +1,17 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (cat.js) to use its database functions.
+var cat = require("../models/cat.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+  cat.all(function(data) {
+    var hbsObject = {
+      cats: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
