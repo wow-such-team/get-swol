49c8bb3a3e6d94e2e7bd88fb9bace7ab7fb41c95
htsiang <htsiang3@gmail.com>
2019-06-15 13:30:29 -0700

    Added seeds.sql file. Added premadeWO table to schema.sql.

--- db/schema.sql
+++ db/schema.sql
@@ -15,5 +15,13 @@ CREATE TABLE users
     id INT NOT NULL AUTO_INCREMENT,
     username VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    PRIMARY KEY(id)
 );
+
+CREATE TABLE premadeWO {
+    id INT NOT NULL,
+    WOtype VARCHAR(255) NOT NULL,
+    exerciseList VARCHAR(255) NOT NULL,
+    PRIMARY KEY(id)
+};
