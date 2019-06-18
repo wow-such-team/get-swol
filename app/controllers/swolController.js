a85646a57099464fc2deeeea8458afc8b60d5ec3
Rebecca Pae <r.pae92@gmail.com>
2019-06-17 20:56:47 -0700

    added favorites layout

--- app/controllers/swolController.js
+++ app/controllers/swolController.js
@@ -8,4 +8,33 @@ router.get("/", function(req, res) {
     res.render("index");
 });
 
+router.get("/favorites", function(req, res) {
+    res.render("favorites");
+});
+
+//Facebook authentication?
+var passport = require('passport')
+  , FacebookStrategy = require('passport-facebook').Strategy;
+
+passport.use(new FacebookStrategy({
+    clientID: "883328152001251",
+    clientSecret: "b374221e853f607b9ef1223ad56fa01f",
+    callbackURL: "http://localhost:8080/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+router.get('/auth/facebook', passport.authenticate('facebook'));
+
+router.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/favorites');
+  });
+
 module.exports = router;
