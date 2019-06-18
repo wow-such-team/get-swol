de44c3f249bf8363ba20fc9d29ccb62a55a114fa
htsiang <htsiang3@gmail.com>
2019-06-17 21:15:01 -0700

    Changed password for connection.

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: process.env.PASSWORD,
+  password: keys.password,
   database: "workout_db"
 });
 
