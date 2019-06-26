1e2178182bd40567a96e239fd580f6dbeabd38b9
Kim Gorcyca <kgorcyca@gmail.com>
2019-06-25 20:27:59 -0700

    appending saved workouts to the date

--- public/assets/js/favorites.js
+++ public/assets/js/favorites.js
@@ -1,55 +1,43 @@
-console.log("favorites.js working");
+$(document).ready(function() {
 
-//  // dynamically create html elements
-//  var userNameDiv = $("<td>", {class:"username", text: childSnapshot.val().name});
-//  var recSaveDiv = $("<td>", {class: "saverecipe", text: "Save Recipe"});
- 
-//  // Employee Info
-//  console.log(userNameDiv.text());
-//  console.log(recSaveDiv.text());
-
-//  // Create the new row
-//  var newRow = $("<tr>").append(
-//      userNameDiv,
-//      recSaveDiv
-//  );
-
-//  // give row attribute to be later referenced
-//  newRow.attr("name", userNameDiv.text());
-
-//  // Append the new row to the table
-//  $("#user-table > tbody").append(newRow);
-
-    // when click date, show user's favorites 
-    $("#button1").on("click", function() {
-        console.log("clicked save");
-        // console.log($(this).text());
+    console.log("favorites.js working");
+    
+    var savedWorkouts= [
         
-        // // if clicked item text is the same as name value of data
-        // if($(this).text()===childSnapshot.val().name) {
-        //     console.log("it worked");
-
-        //     console.log(childSnapshot.val().name);
-
-        //     // clear previously displayed recipes
-        //     $("#recipe-table-body").html("");
-
-        //     // update title of box to show which user's favorites are being displayed
-        //     $("#user-favorites-title").text("Favorite Recipes for " + childSnapshot.val().name);
-
-        //     // get user's favorite recipes from firebase
-        //     database.ref('users').child(childSnapshot.key).child('recipes').on('child_added', function(childSnapshot) {
-        //         console.log(childSnapshot.val());
-
-        //         // dynamically create elements w/ text to display recipe title & url
-        //         var favRecipeTitle = $("<td>", {text: childSnapshot.val().label});
-        //         var favRecipeLink = $("<a>", {href: childSnapshot.val().url, text: childSnapshot.val().url});
-        //         var linkDiv = $("<td>").append(favRecipeLink);
-            
-        //         var newRecipeRow = $("<tr>").append(favRecipeTitle, linkDiv);
-
-        //         $("#recipe-table > tbody").append(newRecipeRow);;
-        //     });
-        // };
+    ];
+    
+    var savedWODiv = $('<th scope="col"> ❤' + savedWorkouts + '</th>');
+       
+    
+    $("#sunday_btn").on("click", function(){
+        $("#sunday_data").append(savedWODiv)
+       })
+    
+    
+    $("#monday_btn").on("click", function(){
+        $("#monday_data").append(savedWODiv)
+        })
+    
+    
+    $("#tuesday_btn").on("click", function(){
+        $("#tuesday_data").append(savedWODiv)
+        })
+    
+    $("#wednesday_btn").on("click", function(){
+        $("#wednesday_data").append(savedWODiv)
+        })
+    
+    $("#thursday_btn").on("click", function(){
+        $("#thursday_data").append(savedWODiv)
+        })
+    
+    $("#friday_btn").on("click", function(){
+        $("#friday_data").append(savedWODiv)
+        })
+    
+    $("#saturday_btn").on("click", function(){
+        $("#saturday_data").append(savedWODiv)
+        })
+    
     });
-
+    
